858e83fc3cbb47a34886695ec61340015e57d3ed
XxgamingxXyt12 <158865391+XxgamingxXyt12@users.noreply.github.com>
2026-08-20 18:57:33 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "RecReload", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "Playing RecReload",     // this is what people actually see
         type: 0,               // Custom
       },
     ],
